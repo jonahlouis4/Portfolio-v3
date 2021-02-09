e5527f50f713a3416cb2abda518adac111133e8b
UrLover <jonah_louis@outlook.com>
2021-02-09 12:52:37 -0500

    :heavy_check_mark: Added icons

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -12,10 +12,10 @@ const ProjectDisplay = ({iconName, projectName, description, github, url}) => {
     function iconDisplay() {
         if (iconName === "faReact") {
             // Color = #00d8ff
-            return <FontAwesomeIcon icon={faReact} size="6x"/>
+            return <FontAwesomeIcon icon={faReact} size="4x"/>
         } else if (iconName === "faPhp") {
             // Color = #8993be
-            return <FontAwesomeIcon icon={faPhp} size="6x" style={{}}/>
+            return <FontAwesomeIcon icon={faPhp} size="4x"/>
         }
     }
 
@@ -28,7 +28,7 @@ const ProjectDisplay = ({iconName, projectName, description, github, url}) => {
             ]}
         >
             <div 
-            style={{textAlign: 'center', paddingBottom: '20px'}}
+            style={{textAlign:'center', paddingBottom:'20px', color:'#434343'}}
             >
                 {iconDisplay()}
             </div>
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact, faJsSquare, faAngular } from '@fortawesome/free-brands-svg-icons'
+import { faReact, faJsSquare, faWordpress, faBootstrap } from '@fortawesome/free-brands-svg-icons'
 import { Typography, Col, Row } from 'antd';
 
 const { Title } = Typography;
@@ -26,7 +26,32 @@ const Technologies = () => {
             <div
             style={{paddingTop:'50px', border: '1px solid red'}}
             >
-                
+                {/* Top 4 technologies */}
+                <Row
+                justify="space-between"
+                style={{textAlign:'center'}}
+                >
+                    <Col>
+                        <FontAwesomeIcon icon={faJsSquare} size="6x" style={{color:"#434343"}}/>
+                        <Title level={5}>JavaScript</Title>
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faReact} size="6x" style={{color:"#434343"}}/>
+                        <Title level={5}>React JS</Title>
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faWordpress} size="6x" style={{color:"#434343"}}/>
+                        <Title level={5}>Wordpress</Title>
+                    </Col>
+                    <Col>
+                        <FontAwesomeIcon icon={faBootstrap} size="6x" style={{color:"#434343"}}/>
+                        <Title level={5}>Boostrap</Title>
+                    </Col>
+                </Row>
+                {/* Other technologies and libraries/framework  */}
+                <Row>
+                    
+                </Row>
             </div>
         </div>
     )
