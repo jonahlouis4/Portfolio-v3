057dc58923c3cf68107240c09a88d3d33b1b7ead
UrLover <jonah_louis@outlook.com>
2021-02-18 15:47:31 -0500

    :heavy_check_mark: Added animations

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { motion } from 'framer-motion'
 import { Row, Col, Typography, Button, Avatar, Space } from 'antd'
 import { GithubOutlined } from '@ant-design/icons'
 import user from '../images/jonah.jpg'
 
 const { Title } = Typography;
 
+// Styles
 const headerTitle = {
     fontSize: '56px',
     fontWeight: '500',
@@ -24,29 +26,57 @@ const colorFont = {
     fontWeight: '500'
 }
 
+// Variants
+const contVariant = {
+    visible: { opacity: 1,
+        transition: { delay:1.8, when:"beforeChildren", staggerChildren: 0.2 },
+    },
+    hidden: { opacity: 0 }
+}
+
+const elementVariant = {
+    visible: { opacity:1,
+        transition: { duration: 1 },
+    },
+    hidden: { opacity: 0 }
+}
+
 const Introduction = () => {
     return (
-        <div
+        <motion.div
         style={{margin: '200px 0'}}
         className="introduction-container"
+        initial="hidden"
+        animate="visible"
+        variants={contVariant}
         >
             <Row>
                 <Col 
                 xl={18} 
                 style={{ margin:'auto'}}
                 >
-                    <Title style={headerTitle}>I'm Jonah,</Title>
-                    <Title level={3} style={subHeaderTitle} type="secondary">
-                        I love to <span style={colorFont}>learn</span> and <span style={colorFont}>build</span> things for the web.
-                    </Title>
+                    <motion.div variants={elementVariant}>
+                        <Title style={headerTitle}>I'm Jonah,</Title>
+                    </motion.div>
+                    <motion.div variants={elementVariant}>
+                        <Title level={3} style={subHeaderTitle} type="secondary">
+                            I love to <span style={colorFont}>learn</span> and <span style={colorFont}>build</span> things for the web.
+                        </Title>
+                    </motion.div>
                     <div
                     style={{marginTop: '10px'}}
                     >
                         <Space size="small">
-                            <Button href="mailto:jonah_louis@outlook.com">Contact me</Button>
-                            <a href="https://github.com/jonahlouis4" target="_blank">
+                            <motion.div variants={elementVariant}>
+                                <Button href="mailto:jonah_louis@outlook.com">Contact me</Button>   
+                            </motion.div>
+                            <motion.a 
+                            href="https://github.com/jonahlouis4" 
+                            target="_blank"
+                            variants={elementVariant}
+                            >
                                 <Button shape="circle" icon={<GithubOutlined />} />
-                            </a>
+                            </motion.a>
                         </Space>
                         
                     </div>
@@ -57,13 +87,15 @@ const Introduction = () => {
                 style={{ margin:'auto'}}
                 className="intro-picture"
                 >
-                    <Avatar
-                        size={{ xs: 180, sm: 180, md: 180, lg: 180, xl: 220, xxl: 250 }}
-                        src={user}
-                    />
+                    <motion.div variants={elementVariant}>
+                        <Avatar
+                            size={{ xs: 180, sm: 180, md: 180, lg: 180, xl: 220, xxl: 250 }}
+                            src={user}
+                        />
+                    </motion.div>
                 </Col>
             </Row> 
-        </div>
+        </motion.div>
     )
 }
 
