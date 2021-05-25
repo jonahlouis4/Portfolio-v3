0e919528225a4892cae2c336ea6d9270382fd038
UrLover <jonah_louis@outlook.com>
2021-05-25 17:51:03 -0400

    Added Contributions in SideNav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -55,6 +55,7 @@ const SideNav = ({isOpen, sideNavControl, sectionName, sectionLink}) => {
     return (
         <div style={sideNavStyle} className={isOpen ? navOpenClass : navCloseClass}>
             <CloseOutlined style={closeBtn} onClick={navControl} className="options"/>
+            <a href={'#'+sectionLink.section0}><Text type="secondary" className="options">{sectionName.section0}</Text></a>
             <a href={'#'+sectionLink.section1}><Text type="secondary" className="options">{sectionName.section1}</Text></a>
             <a href={'#'+sectionLink.section2}><Text type="secondary" className="options">{sectionName.section2}</Text></a>
             <a href={Resume} target="_blank" rel='noopener noreferrer'>
